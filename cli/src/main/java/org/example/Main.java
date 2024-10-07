13ea1c58ca72524c5b6ba4b2d5420b28461d3a5b
john <mustchjo@tutanota.com>
2024-10-07 15:43:44 +0100

    users task complete

--- cli/src/main/java/org/example/Main.java
+++ cli/src/main/java/org/example/Main.java
@@ -1,7 +1,54 @@
 package org.example;
 
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Path filePath;
+        List<String> lines = new ArrayList<>();
+        String jsonString;
+
+        String fileName = "/reports/users.json";
+
+        try {
+            filePath = Paths.get("reports", "users.json");
+            lines = Files.readAllLines(filePath);
+
+            jsonString = String.join("", lines);
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            List<User> users = objectMapper.readValue(jsonString, new TypeReference<List<User>>() {});
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for(User user : users){
+                stringBuilder.append(
+                        String.format("INSERT INTO users VALUES (%s, \"%s\", \"%s\", \"%s\"," +
+                                "\"%s\", \"%s\", \"%s\"); \n", user.id, user.username, user.first_name,
+                                user.last_name, user.email, user.avatar, user.password)
+                                .replaceAll("'", "''")
+                );
+            }
+
+            Path outPath = Paths.get("db", "seeds", "users.sql");
+            Files.write(outPath, stringBuilder.toString().getBytes());
+        } catch (Exception e) {
+            System.out.println("something went wrong!!");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void getUsers(){
+
     }
 }
