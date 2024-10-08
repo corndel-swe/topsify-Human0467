600c5185cc0a65534b8b1d9f1cffc7f8fb36f7d2
Human0467 <mustchjo@gmail.com>
2024-10-08 10:44:16 +0100

    6a passes

--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,3 +2,8 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
+SELECT artists.name, COUNT(albums.name)
+FROM artists JOIN albums
+ON artists.id = albums.artist_id
+GROUP BY albums.artist_id
+ORDER BY artists.name ASC;
